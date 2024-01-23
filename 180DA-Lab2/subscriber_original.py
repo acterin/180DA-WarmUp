695f69a0417534a16818cb5079620366f5274270
acterin <108167879+acterin@users.noreply.github.com>
2024-01-22 19:01:38 -0800

    publish

--- 180DA-Lab2/subscriber_original.py
+++ 180DA-Lab2/subscriber_original.py
@@ -1,4 +1,6 @@
 import paho.mqtt.client as mqtt
+
+
 # 0. define callbacks - functions that run when events happen.
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
